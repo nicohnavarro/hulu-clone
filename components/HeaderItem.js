8e69e88b5fde61c283d9ed7b57a01ec891c4ade9
nicohnavarro <nicohnavarro@gmail.com>
2021-08-19 02:43:22 -0300

    :hammer: add thumbnail

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,7 +1,7 @@
 export default function HeaderItem({ title, Icon }) {
   return (
     <div className="flex  flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
-      <Icon className="h-8 mb-1 group-hover:animate-spin" />
+      <Icon className="h-8 mb-1 group-hover:animate-spin hover:scale-125" />
       <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
     </div>
   );
--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,23 @@
-import urls from '../utils/request';
+import requests from "../utils/requests";
+import { useRouter } from "next/dist/client/router";
 
 export default function Nav() {
-  return (
-    <nav>
-      <div>
-        {urls.map((url)=>
+  const router = useRouter();
 
-        <p key={url.title}>{url.title}</p>
-        )}
+  return (
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+            key={key}
+            onClick={() => router.push(`/?genre=${key}`)}
+          >
+            {title}
+          </h2>
+        ))}
       </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"/>
     </nav>
-  )
+  );
 }
